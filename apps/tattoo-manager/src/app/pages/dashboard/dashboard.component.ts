b10dbb2f3db170f6ec9e944c89c5cdb20784744d
kirill-borisenok <boravdv@mail.ru>
2023-05-08 17:30:40 +0300

    feature: add autocomplete

--- apps/tattoo-manager/src/app/pages/dashboard/dashboard.component.ts
+++ apps/tattoo-manager/src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,10 @@
+import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   OnInit,
   inject,
 } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { StepNames } from './entities/dashboard.constants';
-import { MatButtonModule } from '@angular/material/button';
 import {
   FormBuilder,
   FormControl,
@@ -15,8 +12,12 @@ import {
   FormsModule,
   ReactiveFormsModule,
 } from '@angular/forms';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MomentDateAdapter,
+} from '@angular/material-moment-adapter';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import {
   DateAdapter,
@@ -24,13 +25,18 @@ import {
   MAT_DATE_LOCALE,
   MatNativeDateModule,
 } from '@angular/material/core';
-import {
-  MomentDateAdapter,
-  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
-} from '@angular/material-moment-adapter';
-import { GoogleService } from '../../services/google.service';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import * as moment from 'moment';
+import { Observable, map, startWith } from 'rxjs';
 import { GoogleSheetService } from '../../services/google-sheet.service';
+import { GoogleService } from '../../services/google.service';
+import { StepNames } from './entities/dashboard.constants';
 
+@UntilDestroy()
 @Component({
   standalone: true,
   selector: 'dashboard',
@@ -46,6 +52,8 @@ import { GoogleSheetService } from '../../services/google-sheet.service';
     MatDatepickerModule,
     MatNativeDateModule,
     MatButtonToggleModule,
+    MatAutocompleteModule,
+    CommonModule
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [
@@ -74,20 +82,31 @@ export class DashboardComponent implements OnInit {
   protected readonly stepNames = StepNames;
   protected readonly btnName = 'Сохранить';
 
+  protected cities!: string[];
+  protected names!: string[];
+  protected sources!: string[];
+  protected status!: string[];
+
+  protected filteredSources!: Observable<string[]>;
+  protected filteredNames!: Observable<string[]>;
+  protected filteredStatus!: Observable<string[]>;
+  protected filteredCities!: Observable<string[]>;
+
+  // DI
   private readonly formBuilder = inject(FormBuilder);
   private readonly googleService = inject(GoogleService);
   private readonly googleSheetService = inject(GoogleSheetService);
 
   // Form build
   protected applicationDateFormGroup: FormGroup = this.formBuilder.group({
-    applicationDate: [''],
+    applicationDate: [moment()],
   });
   protected nameFormGroup: FormGroup = this.formBuilder.group({ name: [''] });
   protected maleFormGroup: FormGroup = this.formBuilder.group({
     male: ['Мужской'],
   });
   protected sessionDateFormGroup: FormGroup = this.formBuilder.group({
-    sessionDate: [''],
+    sessionDate: [moment()],
   });
   protected linkFormGroup: FormGroup = this.formBuilder.group({ link: [''] });
   protected sourceFormGroup: FormGroup = this.formBuilder.group({
@@ -118,7 +137,36 @@ export class DashboardComponent implements OnInit {
       this.googleService.initClient();
     }, 1000);
 
-    this.googleSheetService.loadSheet().subscribe((item) => console.log(item));
+    this.googleSheetService.loadSheet().pipe(untilDestroyed(this)).subscribe((item) => {
+      this.cities = [...new Set(item.map((el) => el[this.stepNames.City]))].filter((item) => item);
+      this.names = [...new Set(item.map((el) => el[this.stepNames.Name]))].filter((item) => item);
+      this.sources = [...new Set(item.map((el) => el[this.stepNames.Source]))].filter((item) => item);
+      this.status = [...new Set(item.map((el) => el[this.stepNames.Status]))].filter((item) => item);
+
+      this.filteredSources = this.sourceFormGroup.controls.source.valueChanges.pipe(
+        untilDestroyed(this),
+        startWith(''),
+        map(value => this.filterSources(value || '')),
+      );
+
+      this.filteredNames = this.nameFormGroup.controls.name.valueChanges.pipe(
+        untilDestroyed(this),
+        startWith(''),
+        map(value => this.filterSources(value || '')),
+      )
+
+      this.filteredStatus = this.statusFormGroup.controls.status.valueChanges.pipe(
+        untilDestroyed(this),
+        startWith(''),
+        map(value => this.filterSources(value || '')),
+      )
+
+      this.filteredCities = this.cityFormGroup.controls.city.valueChanges.pipe(
+        untilDestroyed(this),
+        startWith(''),
+        map(value => this.filterSources(value || '')),
+      )
+    });
   }
 
   protected getToken(): void {
@@ -133,5 +181,11 @@ export class DashboardComponent implements OnInit {
     this.googleSheetService
       .addNewValue()
       .subscribe((item) => console.log(item));
+  }
+
+  private filterSources(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.sources.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
--- apps/tattoo-manager/src/app/services/google-sheet.mapper.ts
+++ apps/tattoo-manager/src/app/services/google-sheet.mapper.ts
@@ -3,7 +3,8 @@ import { ExcelResponse } from '@tattoo-manager/shared/interfaces/common.interfac
 export class GoogleSheetMapper {
   public static getExcelDataMap(data: ExcelResponse): Record<string, string>[] {
     const titles = data.values[0];
-    return data.values.map((item) =>
+    
+    return data.values.slice(1).map((item) =>
       titles.reduce((acc, el, index) => ({ ...acc, [el]: item[index] }), {})
     );
   }
